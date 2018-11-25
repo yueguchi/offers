6eb557fef6ff5bd41c5a3ee3191ee8137c1df77e
eguchi <eguchi@asial.co.jp>
2018-11-25 17:12:20 +0900

    cognitouser登録とアプリuser連携

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -71,20 +71,19 @@ export default {
   mounted() {},
   methods: {
     async signup() {
+      // userSub=認証された一意の識別子(UUID)
       const { userSub } = await Auth.signUp({
         username: this.userInfo.email,
         password: this.userInfo.password
       })
-      console.log(userSub)
       if (!userSub) throw new Error('signup failed.')
-      // 認証された一意の識別子(UUID)
       this.userInfo.sub = userSub
       this.isConfirmStep = true
     },
     async confirm() {
       await Auth.confirmSignUp(this.userInfo.email, this.userInfo.code)
       // システムにも登録
-      if (!this.sub) throw new Error('Sub is not found.')
+      if (!this.userInfo.sub) throw new Error('Sub is not found.')
       // api.putUser({
       //   email: this.userInfo.email,
       //   sub: this.userInfo.sub
